dfb047b4191f64803cdd191696f131f81e58706e
Roubako <100596673+Roubako@users.noreply.github.com>
2022-04-19 15:27:46 +0800

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -41,8 +41,19 @@ function displayWeather(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
+function search(city) {
+  let apiKey = "fd829bcd869681d406f6ef74fa956525";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
 
-let apiKey = "fd829bcd869681d406f6ef74fa956525";
-let city = "Dubai";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayWeather);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
